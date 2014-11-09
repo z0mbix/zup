a754931f18cabe0e300aeceae19d82cc153e132e
David Wooldridge (z0mbix) <zombie@zombix.org>
2014-11-09 23:51:59 +0000

    Added rpm build (will this work on travis ci?)

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 fpm -s dir -t deb -n zup -v ${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT:0:7} --prefix /usr/local/bin zup
+fpm -s dir -t rpm -n zup -v ${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT:0:7} --prefix /usr/local/bin zup
