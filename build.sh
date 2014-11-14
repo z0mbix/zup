4c76f3d2ba055d33445da0b59fc3e1dfac2ee3c6
David Wooldridge (z0mbix) <zombie@zombix.org>
2014-11-14 00:28:48 +0000

    Added rpm building support

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 mkdir -p build
 cd build
 fpm -s dir -t deb -n zup -C .. -v ${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT:0:7} --prefix /usr/local/bin zup
+fpm -s dir -t rpm -n zup -C .. -v ${TRAVIS_BUILD_NUMBER}-${TRAVIS_COMMIT:0:7} --prefix /usr/local/bin zup
